4467000d3d4ec0f5071fba9c89586189e74208ad
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-06-11 00:18:32 +0530

    .

--- random programs/palindrome.c
+++ random programs/palindrome.c
@@ -2,11 +2,12 @@
 int palin(int n)
 {
     int rem, s = 0;
-    for (int x = n; x > 0;x=x/10){
+    for (int x = n; x > 0; x = x / 10)
+    {
         rem = n % 10;
         s = (s * 10) + rem;
     }
-    if(n==s)
+    if (n == s)
         return 1;
     else
         return 0;
@@ -16,7 +17,7 @@ int main()
     int n;
     printf("\nEnter Range : ");
     scanf("%d", &n);
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if (palin(i))
             printf("%d ", i);
