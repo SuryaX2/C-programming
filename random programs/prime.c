2aa7519b6df9672991715cbfe3322fda9e1df428
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-06-10 22:23:47 +0530

    .

--- random programs/prime.c
+++ random programs/prime.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
-void prime(int n){
-
+int prime(int n){
+    int f = 1;
+    if(n%2==0){
+        f = 0;
+        return f;
+    }
+    else
+        return f;
 }
 int main()
 {
     int n;
     printf("\nEnter Number : ");
     scanf("%d", &n);
+    for (int i = 1; i < n;i++){
+        if (prime(i)){
+            printf("%d ", n);
+        }
+    }
+
     return 0;
 }
