04c0a818889b66ccfc35b6d84b1131017487b1c0
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-06-11 12:48:35 +0530

    .

--- random programs/pat.c
+++ random programs/pat.c
@@ -1,9 +1,27 @@
-#include<stdio.h>
-void pattern(int n){
-    for (int i = 1; i <= n;i++){
-        for (int j = n; j >= i;j--)
-            printf("%d",i);
+#include <stdio.h>
+void pattern1(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+            printf("%d", i);
         printf("\n");
+    }
+}
+void pattern2(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            char c = 'A' + i;
+            for (int j = 0; j <= i; j++)
+            {
+                printf("%c ", c);
+                c++;
+            }
+            printf("\n");
+        }
     }
 }
 int main()
@@ -11,6 +29,7 @@ int main()
     int n;
     printf("\nEnter Range : ");
     scanf("%d", &n);
-    pattern(n);
+    pattern1(n);
+    pattern2(n);
     return 0;
 }
