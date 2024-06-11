7269effed1e4cf1797ddf91d3b6beee2c0bcaf6d
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-06-11 10:32:47 +0530

    .

--- random programs/pat.c
+++ random programs/pat.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 void pattern(int n){
-    
+    for (int i = 1; i <= n;i++){
+        for (int j = n; j >= i;j--)
+            printf("*");
+        printf("\n");
+    }
 }
 int main()
 {
