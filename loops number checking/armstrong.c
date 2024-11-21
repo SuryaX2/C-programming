ffbb787ff881e187b0ed8ffef72c7cd51cb6c6d2
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-11-21 17:14:08 +0530

    Armstrong without POW()

--- loops number checking/armstrong.c
+++ loops number checking/armstrong.c
@@ -1,24 +1,28 @@
-// check whether a no. armstrong or not
 #include <stdio.h>
-#include <math.h>
+
+int power(int a, int b) {
+    int p = 1, i;
+    for (i = 1; i <= b; ++i) {
+        p = p * a;
+    }
+    return p;
+}
 int main()
 {
-	int n, c, rem, p, s = 0, x;
-	printf("Enter Number : ");
-	scanf("%d", &n);
-	for (x = n; n > 0; n = n / 10)
-	{
-		c++;
-	}
-	for (n = x; n > 0; n = n / 10)
-	{
-		rem = n % 10;
-		p = pow(rem, c);
-		s = s + p;
-	}
-	if (s == x)
-		printf("%d is ARMSTRONG no.", x);
-	else
-		printf("%d is not ARMSTRONG no.", x);
-	return 0;
+    int n, c, rem, p, s = 0, x;
+    printf("Enter Number : ");
+    scanf("%d", & n);
+    for (x = n; n > 0; n = n / 10)
+        c++;
+    for (n = x; n > 0; n = n / 10)
+    {
+        rem = n % 10;
+        p = power(rem, c);
+        s = s + p;
+    }
+    if (s == x)
+        printf("%d is ARMSTRONG no.", x);
+    else
+        printf("%d is not ARMSTRONG no.", x);
+    return 0;
 }
