5f746f1d2155517ee80538325d0d733fce4eba78
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-12-08 23:47:09 +0530

    .

---
+++ Interview/Nested Loop/p19.c
@@ -0,0 +1,35 @@
+/*
+    Enter The Number Of Rows : 4
+    1     1
+    12   21
+    123 321
+    12344321
+*/
+#include <stdio.h>
+void printPattern(int n)
+{
+    int sp = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            printf("*");
+        }
+        for (int j = 1; j <= sp; j++)
+            printf(" ");
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            printf("*");
+        }
+        sp += 2;
+        printf("\n");
+    }
+}
+int main()
+{
+    int n;
+    printf("\nEnter The Number Of Rows : ");
+    scanf("%d", &n);
+    printPattern(n);
+    return 0;
+}
