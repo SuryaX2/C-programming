2d1ebdf79898ac604f4b96deed76ef0a203b68a7
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-12-08 23:58:07 +0530

    .

--- Interview/Nested Loop/p19.c
+++ Interview/Nested Loop/p19.c
@@ -24,6 +24,22 @@ void printPattern(int n)
         sp += 2;
         printf("\n");
     }
+    sp = (n - 1) * 2;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        for (int j = 1; j <= sp; j++)
+            printf(" ");
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        sp -= 2;
+        printf("\n");
+    }
 }
 int main()
 {
