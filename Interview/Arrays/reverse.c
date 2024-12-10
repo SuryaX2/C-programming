6631bf055fb12dbc1052d383867da72eca8d6510
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-12-10 18:24:03 +0530

    .

--- Interview/Arrays/reverse.c
+++ Interview/Arrays/reverse.c
@@ -15,15 +15,10 @@ int main()
     printf("\nEnter Elements : ");
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
-    i = 0, j = n - 1;
-    while (i < j)
-    {
+    for (i = 0, j = n - 1; i < j; i++, j--)
         swap(&a[i], &a[j]);
-        i++, j--;
-    }
     printf("\nReversed Array : ");
     for (int i = 0; i < n; i++)
         printf("%d ", a[i]);
-
     return 0;
 }
