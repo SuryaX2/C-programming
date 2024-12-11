3e14a991b77466b17b575352b355ba9720f1b56e
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-12-11 21:31:14 +0530

    .

---
+++ Interview/Arrays/smax.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+int secondLargest(int arr[], int n)
+{
+    if (n < 2)
+        return -1;
+    int max = INT_MIN, smax = INT_MIN;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            smax = max;
+            max = arr[i];
+        }
+        else if (arr[i] > smax && arr[i] != max)
+        {
+            smax = arr[i];
+        }
+    }
+    return smax;
+}
