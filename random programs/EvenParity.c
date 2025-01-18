75c08a91a39edc4995d6a159599b8511d4e60d40
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-01-18 23:13:28 +0530

    Add Even and Odd Parity implementations for error detection

---
+++ random programs/EvenParity.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    printf("\nEnter size of message: ");
+    scanf("%d", &n);
+    char data[n + 2]; // +2 to accommodate parity bit and null terminator
+    printf("\nEnter the message: ");
+    scanf("%s", data);
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (data[i] == '1')
+            count++;
+    }
+    char parityBit = (count % 2 == 0) ? '0' : '1';
+    data[n] = parityBit;
+    data[n + 1] = '\0';
+    printf("Message with parity bit: %s\n", data);
+    return 0;
+}
