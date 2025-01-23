c17fd5abf2127e20f191355ee6da6e851b8c8835
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-01-24 00:59:03 +0530

    Refactor knap01.c to improve variable naming for clarity in the knapsack function

--- random programs/knap01.c
+++ random programs/knap01.c
@@ -3,16 +3,16 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 int knapsack(int W, int wt[], int val[], int n) {
-    int i, w;
+    int i, j;
     int knap[n + 1][W + 1];
     for (i = 0; i <= n; i++) {
-        for (w = 0; w <= W; w++) {
-            if (i == 0 || w == 0)
-                knap[i][w] = 0;
-            else if (wt[i - 1] <= w)
-                knap[i][w] = max(val[i - 1] + knap[i - 1][w - wt[i - 1]], knap[i - 1][w]);
+        for (j = 0; j <= W; j++) {
+            if (i == 0 || j == 0)
+                knap[i][j] = 0;
+            else if (wt[i - 1] <= j)
+                knap[i][j] = max(val[i - 1] + knap[i - 1][j - wt[i - 1]], knap[i - 1][j]);
             else
-                knap[i][w] = knap[i - 1][w];
+                knap[i][j] = knap[i - 1][j];
         }
     }
     return knap[n][W];
