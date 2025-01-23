168de9ec64889b358c13fde0550fb6cc53721567
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-01-24 00:37:59 +0530

    Refactor knap01.c to improve code readability by simplifying the max function and enhancing formatting consistency

--- random programs/knap01.c
+++ random programs/knap01.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-int max(int a, int b){
-    if (a > b) return a;
-    else return b;
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
-int knapsack(int W, int wt[], int val[], int n){
+int knapsack(int W, int wt[], int val[], int n) {
     int i, w;
     int knap[n + 1][W + 1];
-    for (i = 0; i <= n; i++){
-        for (w = 0; w <= W; w++){
-            if (i == 0 || w == 0) 
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
                 knap[i][w] = 0;
             else if (wt[i - 1] <= w)
                 knap[i][w] = max(val[i - 1] + knap[i - 1][w - wt[i - 1]], knap[i - 1][w]);
@@ -18,8 +17,7 @@ int knapsack(int W, int wt[], int val[], int n){
     }
     return knap[n][W];
 }
-int main()
-{
+int main() {
     int val[] = {20, 25, 40};
     int wt[] = {25, 20, 30};
     int W = 50;
