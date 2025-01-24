a7837056a087a259c1c0eee58ec3752eab1b714a
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-01-24 23:07:54 +0530

    Refactor Merge.c and Quick.c to improve code readability by simplifying function definitions and consistent formatting

--- random programs/Merge.c
+++ random programs/Merge.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
-void printArray(int *a, int n)
-{
+void printArray(int *a, int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", a[i]);
     printf("\n");
 }
-void Merge(int *a, int low, int mid, int high)
-{
+void Merge(int *a, int low, int mid, int high) {
     int i, j, k, b[100];
     i = low;
     j = mid + 1;
     k = low;
-    while (i <= mid && j <= high)
-    {
-        if (a[i] < a[j])
-        {
+    while (i <= mid && j <= high) {
+        if (a[i] < a[j]) {
             b[k] = a[i];
             i++;
             k++;
-        }
-        else
-        {
+        } else {
             b[k] = a[j];
             j++;
             k++;
         }
     }
-    while (i <= mid)
-    {
+    while (i <= mid) {
         b[k] = a[i];
         i++;
         k++;
     }
-    while (j <= high)
-    {
+    while (j <= high) {
         b[k] = a[j];
         j++;
         k++;
@@ -41,19 +33,16 @@ void Merge(int *a, int low, int mid, int high)
     for (i = low; i <= high; i++)
         a[i] = b[i];
 }
-void mergeSort(int *a, int low, int high)
-{
+void mergeSort(int *a, int low, int high) {
     int mid;
-    if (low < high)
-    {
+    if (low < high) {
         mid = (low + high) / 2;
         mergeSort(a, low, mid);
         mergeSort(a, mid + 1, high);
         Merge(a, low, mid, high);
     }
 }
-int main()
-{
+int main() {
     int i, n;
     printf("\nEnter Range : ");
     scanf("%d", &n);
--- random programs/Quick.c
+++ random programs/Quick.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-void printArray(int *a, int n)
-{
+void printArray(int *a, int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", a[i]);
     printf("\n");
 }
-int part(int *a, int low, int high)
-{
+int part(int *a, int low, int high) {
     int pivot = a[low], i = low + 1, j = high, temp;
-    do
-    {
+    do {
         while (a[i] <= pivot)
             i++;
         while (a[j] > pivot)
             j--;
-        if (i < j)
-        {
+        if (i < j) {
             temp = a[i];
             a[i] = a[j];
             a[j] = temp;
@@ -26,18 +22,15 @@ int part(int *a, int low, int high)
     a[j] = temp;
     return j;
 }
-void quickSort(int *a, int low, int high)
-{
+void quickSort(int *a, int low, int high) {
     int j;
-    if (low < high)
-    {
+    if (low < high) {
         j = part(a, low, high);
         quickSort(a, low, j - 1);
         quickSort(a, j + 1, high);
     }
 }
-int main()
-{
+int main() {
     int i, n;
     printf("\nEnter Range : ");
     scanf("%d", &n);
