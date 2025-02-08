d72217c96a47863527b5627625f462260e2f5fc2
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-08 22:41:45 +0530

    Add programs to find ASCII values of a character and remove empty Q4.c file

---
+++ Interview/Questions/Q3.c
@@ -0,0 +1,9 @@
+// 3.WAP to find out Ascii values of a character
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    printf("ASCII value of %c is %d\n", ch, ch);
+    return 0;
+}
