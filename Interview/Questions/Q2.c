e0f2e60a84a3ca93fe36e51b79c40ea5c6149bcd
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-08 22:39:56 +0530

    Add program to check if a character is an alphabet

---
+++ Interview/Questions/Q2.c
@@ -0,0 +1,18 @@
+// 2.WAP to check A character is an alphabet or not
+#include <stdio.h>
+int isAlphabet(char ch){
+    return (
+        (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+    ) ? 1 : 0;
+}
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if (isAlphabet(ch)){
+        printf("%c is an alphabet\n", ch);
+    } else {
+        printf("%c is not an alphabet\n", ch);
+    }
+    return 0;
+}
