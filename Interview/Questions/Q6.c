239467501c5da0a4bbfdc56809773000165aa696
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-09 22:28:27 +0530

    Add program to swap two numbers without using a third variable and remove Q5.exe

---
+++ Interview/Questions/Q6.c
@@ -0,0 +1,16 @@
+// 6.WAP to Swap two numbers without third variable
+#include <stdio.h>
+void swap(int *a, int *b) {
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
