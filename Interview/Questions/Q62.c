b1b85ba5a4ff14d2c03825d544e9864a5146ede8
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-20 00:27:52 +0530

    Add program to count distinct elements in an array

---
+++ Interview/Questions/Q62.c
@@ -0,0 +1,29 @@
+// 62.WAP to Count distinct elements of an array
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+int countDistinct(int arr[], int n) {
+    qsort(arr, n, sizeof(int), compare);
+    int count = 1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[i - 1]) {
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int count = countDistinct(arr, n);
+    printf("The number of distinct elements in the array is: %d\n", count);
+    return 0;
+}
