6bc836c5f40f10e87d6112b7bc589a122e223f5b
Surya Sekhar Sharma <141203476+SuryaX2@users.noreply.github.com>
2025-03-20 00:12:14 +0530

    Add Recursive Insertion Sort implementation and fix isSwapped flag in Recursive Bubble Sort

--- Interview/a2z Sheet/Sorting/Recursive_Bubble_Sort.c
+++ Interview/a2z Sheet/Sorting/Recursive_Bubble_Sort.c
@@ -17,6 +17,7 @@ void recursiveBubbleSort(int* a, int n) {
     for (int i = 0; i < n - 1; i++) {
         if (a[i] > a[i + 1]) {
             swap(&a[i], &a[i + 1]);
+            isSwapped = 1;
         }
     }
     if (!isSwapped) return;
---
+++ Interview/a2z Sheet/Sorting/Recursive_Insertion_Sort.c
@@ -0,0 +1,15 @@
+#include "./utility.c"
+
+/**
+ * Recursive Insertion Sort
+ * Algorithm:
+    Insert the element at its correct position in the sorted part of the array
+    Repeat the process for the remaining elements
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ * Best Case, Average Case, Worst Case: O(n^2)
+*/
+
+void recursiveInsertionSort(int* a, int n) {
+    
+}
