fccaab60707f33546802707246a9718d1233500b
Surya Sekhar Sharma <141203476+SuryaX2@users.noreply.github.com>
2025-03-20 00:00:49 +0530

    Implement Quick Sort algorithm with user input functionality

---
+++ Interview/a2z Sheet/Sorting/Quick_Sort.c
@@ -0,0 +1,44 @@
+#include "./utility.c"
+
+/**
+ * Quick Sort
+ * Algorithm:
+    Partition the array into two parts and sort them recursively
+ * Time Complexity: O(nlogn)
+ * Space Complexity: O(logn)
+ * Best Case, Average Case : O(nlogn)
+ * Worst Case : O(n^2)
+*/
+
+int partition(int* a, int low, int high) {
+    int pivot = a[low], i = low, j = high;
+    do {
+        while (a[i] <= pivot) i++;
+        while (a[j] > pivot) j--;
+        if (i < j) swap(&a[i], &a[j]);
+    } while (i < j);
+    swap(&a[low], &a[j]);
+    return j;
+}
+
+void quickSort(int* a, int low, int high) {
+    if (low < high) {
+        int pivot = partition(a, low, high);
+        quickSort(a, low, pivot - 1);
+        quickSort(a, pivot + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter the elements: ");
+    scanArray(a, n);
+    quickSort(a, 0, n - 1);
+    printf("Sorted array: ");
+    printArray(a, n);
+    printf("\n");
+    return 0;
+}
