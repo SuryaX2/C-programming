4793ec871024094d9d4090eccacf0315a17dda3f
Surya Sekhar Sharma <141203476+SuryaX2@users.noreply.github.com>
2025-03-20 00:08:41 +0530

    Implement Recursive Bubble Sort algorithm with user input functionality

---
+++ Interview/a2z Sheet/Sorting/Recursive_Bubble_Sort.c
@@ -0,0 +1,37 @@
+#include "./utility.c"
+
+/**
+ * Recursive Bubble Sort (Optimized)
+ * Algorithm:
+    Compare each element with the next element and swap if the current element is greater than the next element
+    Repeat the process for the remaining elements
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ * Best Case : O(n)
+ * Average Case, Worst Case : O(n^2)
+*/
+
+void recursiveBubbleSort(int* a, int n) {
+    if (n == 1) return;
+    int isSwapped = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            swap(&a[i], &a[i + 1]);
+        }
+    }
+    if (!isSwapped) return;
+    recursiveBubbleSort(a, n - 1);
+}
+
+int main() {
+    int n;
+    printf("\nEnter Number of Elements : ");
+    scanf("%d", &n);
+    int a[n];
+    printf("\nEnter Elements : ");
+    scanArray(a, n);
+    recursiveBubbleSort(a, n);
+    printf("\nSorted Array : ");
+    printArray(a, n);
+    return 0;
+}
